450dada8cb2df554b65770327b560dd84667cebb
patdel0 <zefiru@protonmail.com>
2022-09-02 15:39:12 +0100

    Add lesson 9

---
+++ lessons/09-geometries/src/script.js
@@ -0,0 +1,141 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Float32Array
+// const positionsArray = new Float32Array([
+//   0,
+//   0,
+//   0, // First Vertice
+//   0,
+//   1,
+//   0, // Second Vertice
+//   1,
+//   0,
+//   0, // Third Vertice
+// ])
+
+// // First vertex
+// positionsArray[0] = 0
+// positionsArray[1] = 0
+// positionsArray[2] = 0
+
+// // Second vertex
+// positionsArray[3] = 0
+// positionsArray[4] = 1
+// positionsArray[5] = 0
+
+// // Third vertex
+// positionsArray[6] = 1
+// positionsArray[7] = 0
+// positionsArray[8] = 0
+
+// Object
+const count = 50
+const verticesInTriangle = 3
+const vertexCoordinatesLength = 3 // x, y, z
+const arrayLength = count * verticesInTriangle * vertexCoordinatesLength
+
+const positionsArray = new Float32Array(arrayLength)
+
+for (let i = 0 ; i < arrayLength; i ++) {
+    positionsArray[i] = Math.random();
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position', positionsAttribute)
+
+// const geometry = new THREE.Geometry()
+
+// for (let i = 0; i < 20; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     geometry.vertices.push(
+//       new THREE.Vector3(
+//         (Math.random() - 0.5) * 5,
+//         (Math.random() - 0.5) * 5,
+//         (Math.random() - 0.5) * 5
+//       )
+//     )
+//   }
+//   const verticesIndex = i * 3
+//   geometry.faces.push(
+//     new THREE.Face3(verticesIndex, verticesIndex + 1, verticesIndex + 2)
+//   )
+// }
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
